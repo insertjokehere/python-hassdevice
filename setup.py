ba303e1dcb3c6efdbb58d0cb591e03fe928d0932
William Hughes <whlabratz@gmail.com>
2017-07-17 20:01:10 +1200

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*names, **kwargs):
 
 setup(
     name='hassdevice',
-    version='0.0.1',
+    version='0.0.2',
     license='Apache',
     description='A library for building MQTT devices for HomeAssistant',
     long_description='%s\n%s' % (
